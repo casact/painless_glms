6e727471a67afc47fc11b0e99da3fb94b9fc44e4
Brian Fannin <captain@PirateGrunt.com>
2024-09-12 12:51:44 -0400

    Add license header to source file

--- examples.R
+++ examples.R
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #'===============================================================================
 #' You will need to have the tidyverse, raw and janitor packages installed. We 
 #' only use one function from janitor, so don't need to load it into memory.
